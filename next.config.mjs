e80bf0ed43b9d85d3d5d3add433759513f6948b5
Your Name <babulhossan.info@gmail.com>
2026-04-10 23:50:31 +0600

    complete 3 differance image loaded

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  // https://unsplash.com/photos/lemon-water-in-footed-glass-RgplfXbxLFs
+
+ images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
+
   /* config options here */
   reactCompiler: true,
 };
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,30 @@
+import Image from "next/image";
+import imag from "../../../public/my-photo.jpg";
 
 
 const Home = () => {
     return (
         <div>
+
+            <Image
+            width={500}
+            height={300}
+            src="https://unsplash.com/photos/lemon-water-in-footed-glass-RgplfXbxLFs"
+            alt="helll"
+            />
+
+            {/* <Image
+            src={imag}
+            alt="hello"
+            /> */}
             <h1>home</h1>
+            {/* <Image 
+            width={500}
+            height={300}
+            src="/my-photo.jpg"
+            alt="hello"
+            /> */}
+
         </div>
     );
 };
